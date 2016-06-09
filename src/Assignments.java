2bd62a0f49d7f7896df1a75d62b583e5fcc5d322
341167849 <341167849@109R030573X9Q22.EDU.YRDSB.NET>
2016-06-09 14:42:57 -0400

    Merge origin/master
    Conflicts:
    	src/calculatzarsep2015/StringCalculator.java

--- src/Assignments.java
+++ src/Assignments.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,5 +11,8 @@
  * @author 341167849
  */
 public class Assignments {
-    
+ArrayList<Peer> group = new ArrayList<>();
+Tutor one = new Tutor();
+
+
 }
