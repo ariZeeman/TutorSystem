d3606a0009f99f64157dc12f05d7f01389867297
341167849 <341167849@109R030573X9Q22.EDU.YRDSB.NET>
2016-06-10 14:08:33 -0400

    Merge origin/master
    Conflicts:
    	src/calculatzarsep2015/StringCalculator.java

--- src/InterFace.java
+++ src/InterFace.java
@@ -9,16 +9,22 @@
  */
 public class InterFace extends javax.swing.JFrame {
 
+    boolean subjectCheck = false;
     /**
      * Creates new form InterFace
      */
     public InterFace() {
         initComponents();
         subjectComboBox.removeAllItems();
-        subjectComboBox.addItem("Subject 1");
-        subjectComboBox.addItem("Subject 2");
-        subjectComboBox.addItem("Subject 3");
-        subjectComboBox.addItem("Subject 4");
+        subjectComboBox.addItem("English");
+        subjectComboBox.addItem("Math");
+        subjectComboBox.addItem("Physics");
+        subjectComboBox.addItem("Chemistry");
+        subjectComboBox.addItem("Biology");
+        subjectComboBox.addItem("French");
+        subjectComboBox.addItem("Science");
+        subjectComboBox.addItem("FI French");
+        subjectComboBox.addItem("Other");
     }
 
     /**
@@ -32,12 +38,14 @@ public class InterFace extends javax.swing.JFrame {
 
         buttonGroup1 = new javax.swing.ButtonGroup();
         jTabbedPane1 = new javax.swing.JTabbedPane();
+        jPanel2 = new javax.swing.JPanel();
         jPanel1 = new javax.swing.JPanel();
         findATutorLabel = new javax.swing.JLabel();
         subjectToFindField = new javax.swing.JTextField();
         subjectToFindLabel = new javax.swing.JLabel();
         loginLabel = new javax.swing.JLabel();
-        jPanel2 = new javax.swing.JPanel();
+        confirmButton1 = new javax.swing.JButton();
+        jPanel4 = new javax.swing.JPanel();
         jPanel3 = new javax.swing.JPanel();
         signUpLabel = new javax.swing.JLabel();
         subjectComboBox = new javax.swing.JComboBox();
@@ -48,50 +56,15 @@ public class InterFace extends javax.swing.JFrame {
         wedRadio = new javax.swing.JRadioButton();
         thursRadio = new javax.swing.JRadioButton();
         friRadio = new javax.swing.JRadioButton();
+        confirmationButton2 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        period1Check = new javax.swing.JRadioButton();
+        period1Check1 = new javax.swing.JRadioButton();
+        period1Check2 = new javax.swing.JRadioButton();
+        period1Check3 = new javax.swing.JRadioButton();
+        period1Check4 = new javax.swing.JRadioButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        findATutorLabel.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
-        findATutorLabel.setText("Find a tutor");
-
-        subjectToFindLabel.setText("subject:");
-
-        loginLabel.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
-        loginLabel.setText("Login");
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(subjectToFindLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(subjectToFindField, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(findATutorLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 516, Short.MAX_VALUE)
-                        .addComponent(loginLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(310, 310, 310))))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(loginLabel, javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(findATutorLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(subjectToFindLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(subjectToFindField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(632, Short.MAX_VALUE))
-        );
-
-        jTabbedPane1.addTab("Find a Tutor", jPanel1);
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -105,6 +78,74 @@ public class InterFace extends javax.swing.JFrame {
         );
 
         jTabbedPane1.addTab("Become a Tutor", jPanel2);
+
+        findATutorLabel.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        findATutorLabel.setText("Find a tutor");
+
+        subjectToFindLabel.setText("Subject:");
+
+        loginLabel.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        loginLabel.setText("Login");
+
+        confirmButton1.setText("Confirm");
+        confirmButton1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                confirmButton1MouseClicked(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
+        jPanel4.setLayout(jPanel4Layout);
+        jPanel4Layout.setHorizontalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        jPanel4Layout.setVerticalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 218, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(findATutorLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 516, Short.MAX_VALUE)
+                        .addComponent(loginLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(310, 310, 310))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(confirmButton1)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(jPanel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(subjectToFindLabel, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(subjectToFindField, javax.swing.GroupLayout.DEFAULT_SIZE, 111, Short.MAX_VALUE))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(loginLabel, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(findATutorLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(subjectToFindLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(subjectToFindField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(confirmButton1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(379, Short.MAX_VALUE))
+        );
+
+        jTabbedPane1.addTab("Find a Tutor", jPanel1);
 
         signUpLabel.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         signUpLabel.setText("Sign up");
@@ -125,6 +166,20 @@ public class InterFace extends javax.swing.JFrame {
 
         friRadio.setText("Friday");
 
+        confirmationButton2.setText("Confirm");
+
+        jLabel1.setText("Periods available:");
+
+        period1Check.setText("Period 1");
+
+        period1Check1.setText("Period 1");
+
+        period1Check2.setText("Period 1");
+
+        period1Check3.setText("Period 1");
+
+        period1Check4.setText("Period 1");
+
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
         jPanel3.setLayout(jPanel3Layout);
         jPanel3Layout.setHorizontalGroup(
@@ -132,22 +187,39 @@ public class InterFace extends javax.swing.JFrame {
             .addGroup(jPanel3Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel3Layout.createSequentialGroup()
-                        .addComponent(monRadio)
-                        .addGap(18, 18, 18)
-                        .addComponent(tuesRadio)
-                        .addGap(18, 18, 18)
-                        .addComponent(wedRadio)
-                        .addGap(18, 18, 18)
-                        .addComponent(thursRadio)
-                        .addGap(18, 18, 18)
-                        .addComponent(friRadio))
                     .addComponent(signUpLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                         .addComponent(subjectLabel, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(subjectComboBox, javax.swing.GroupLayout.Alignment.LEADING, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addComponent(availabilityLabel))
-                .addContainerGap(640, Short.MAX_VALUE))
+                    .addComponent(availabilityLabel)
+                    .addComponent(confirmationButton2)
+                    .addGroup(jPanel3Layout.createSequentialGroup()
+                        .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel3Layout.createSequentialGroup()
+                                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                    .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addGroup(jPanel3Layout.createSequentialGroup()
+                                        .addComponent(monRadio)
+                                        .addGap(18, 18, 18)
+                                        .addComponent(tuesRadio)))
+                                .addGap(18, 18, 18)
+                                .addComponent(wedRadio))
+                            .addGroup(jPanel3Layout.createSequentialGroup()
+                                .addComponent(period1Check)
+                                .addGap(18, 18, 18)
+                                .addComponent(period1Check2)
+                                .addGap(18, 18, 18)
+                                .addComponent(period1Check3)))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(period1Check1)
+                            .addGroup(jPanel3Layout.createSequentialGroup()
+                                .addComponent(thursRadio)
+                                .addGap(18, 18, 18)
+                                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(period1Check4)
+                                    .addComponent(friRadio))))))
+                .addContainerGap(630, Short.MAX_VALUE))
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -167,7 +239,18 @@ public class InterFace extends javax.swing.JFrame {
                     .addComponent(wedRadio)
                     .addComponent(thursRadio)
                     .addComponent(friRadio))
-                .addContainerGap(555, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(period1Check)
+                    .addComponent(period1Check2)
+                    .addComponent(period1Check3)
+                    .addComponent(period1Check1)
+                    .addComponent(period1Check4))
+                .addGap(23, 23, 23)
+                .addComponent(confirmationButton2)
+                .addContainerGap(447, Short.MAX_VALUE))
         );
 
         jTabbedPane1.addTab("Become a peer", jPanel3);
@@ -185,6 +268,13 @@ public class InterFace extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void confirmButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_confirmButton1MouseClicked
+        // TODO add your handling code here:
+//        if (){
+//        subjectCheck = true;    
+//        }
+    }//GEN-LAST:event_confirmButton1MouseClicked
 
     /**
      * @param args the command line arguments
@@ -224,14 +314,23 @@ public class InterFace extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel availabilityLabel;
     private javax.swing.ButtonGroup buttonGroup1;
+    private javax.swing.JButton confirmButton1;
+    private javax.swing.JButton confirmationButton2;
     private javax.swing.JLabel findATutorLabel;
     private javax.swing.JRadioButton friRadio;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel4;
     private javax.swing.JTabbedPane jTabbedPane1;
     private javax.swing.JLabel loginLabel;
     private javax.swing.JRadioButton monRadio;
+    private javax.swing.JRadioButton period1Check;
+    private javax.swing.JRadioButton period1Check1;
+    private javax.swing.JRadioButton period1Check2;
+    private javax.swing.JRadioButton period1Check3;
+    private javax.swing.JRadioButton period1Check4;
     private javax.swing.JLabel signUpLabel;
     private javax.swing.JComboBox subjectComboBox;
     private javax.swing.JLabel subjectLabel;
